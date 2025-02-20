91135f76733f830245d10019a63bdfb58c76ec5b
Jack Bramley <s242106@students.aie.edu.au>
2025-02-20 13:57:01 +1100

    Commit 2

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main()
 	std::cout << "welcome back to episode 78 of my github lets play";
 	std::cout << "more changes so i can push to githubby";
 	std::cout << "WOAH DANGEROUS CHANGES HERE GUYS DONT PUSH TO MAIN HOLY MOLY WATCH OUT GUYS DANGER DANGER DANGER";
-	std::cout << "idk some stuff i need a changed commit sooooooooooooooo";
+	std::cout << "different text here because ya know, merge conflicts and stuff i guess";
 
 	return 0
 }
