d666a85c6939130701e64f69d1c8a60b72d5ddf3
Jack Bramley <s242106@students.aie.edu.au>
2025-02-20 11:36:55 +1100

    WARNING: DANGEROUS COMMIT INCOMING IN T-10 SECONDS!

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main()
 	std::cout << "ummmm some other stuff here i guess";
 	std::cout << "welcome back to episode 78 of my github lets play";
 	std::cout << "more changes so i can push to githubby";
+	std::cout << "WOAH DANGEROUS CHANGES HERE GUYS DONT PUSH TO MAIN HOLY MOLY WATCH OUT GUYS DANGER DANGER DANGER";
 
 	return 0
 }
